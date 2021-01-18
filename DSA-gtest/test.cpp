c686c58decca0ba46ae992a54a840b29c2ed144c
Rajiv Albino <rajivalbino@gmail.com>
2021-01-18 17:17:34 -0300

    VS files

--- DSA-gtest/test.cpp
+++ DSA-gtest/test.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+TEST(TestCaseName, TestA)
+{
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, TestB)
+{
+	EXPECT_EQ(2, 2);
+}
+
+/*
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
+*/
