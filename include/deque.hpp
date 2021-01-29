0fb977b04d391d5a24f67924cfbac67331599c6f
Rajiv Albino <rajivalbino@gmail.com>
2021-01-28 21:54:50 -0300

    some methods

--- include/deque.hpp
+++ include/deque.hpp
@@ -1,1 +1,22 @@
+// Deque
+// int datatype
+
+#include "doubly-linked-list.hpp"
+
 #pragma once
+
+namespace rds {
+
+	class Deque {
+	private:
+		DoublyLinkedList list;
+
+	public:
+		Deque() {}
+		~Deque() { list.clear(); }
+
+		inline bool isEmpty()	const { return list.isEmpty(); }
+		inline int  size()		const { return list.size_(); }
+		// ...
+	};
+}
