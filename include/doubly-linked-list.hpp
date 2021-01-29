0fe723a876f80fe66a514912beb0e8116537f386
Rajiv Albino <rajivalbino@gmail.com>
2021-01-28 21:41:07 -0300

    imp: addHead/Tail/At, removeHead/Tail

--- include/doubly-linked-list.hpp
+++ include/doubly-linked-list.hpp
@@ -44,12 +44,74 @@ namespace rds {
 			size = 0;
 		}
 
-		// addHead
-		// addTail
-		// addAt
-		// removeHead
-		// removeTail
+		void addHead(int d) {
+			if (isEmpty())
+				head = tail = new Node(d);
+			else {
+				head = new Node(d, nullptr, head);
+				head->next->prev = head;
+			}
+			size++;
+		}
+
+		void addTail(int d) {
+			if (isEmpty())
+				tail = head = new Node(d);
+			else {
+				tail = new Node(d, tail, nullptr);
+				tail->prev->next = tail;
+			}
+			size++;
+		}
+
+		void addAt(int idx, int d) {
+			if (idx < 0) { /*throw bad index*/ return; }
+			if (idx == 0) { addHead(d); return; }
+			if (idx == size) { addTail(d); return; }
+			if (idx > size) { /*throw cant access*/ return; }
+
+			auto ptr = head;
+			for (int i = 0; i < idx - 1; i++)
+				ptr = ptr->next;
+
+			auto temp = new Node(d, ptr, ptr->next);
+			ptr->next = temp;
+			temp->next->prev = temp;
+			size++;
+		}
+
+		int removeHead() {
+			if (!isEmpty()) {
+				auto temp = head;
+				auto tdata = head->data;
+				head = head->next;
+				head->prev = nullptr;
+				delete temp;
+				size--;
+
+				if (isEmpty()) tail = nullptr;
+				return tdata;
+			}
+			/*else throw empty list*/
+		}
+
+		int removeTail() {
+			if (!isEmpty()) {
+				auto temp = tail;
+				auto tdata = tail->data;
+				tail = tail->prev;
+				tail->next = nullptr;
+				delete temp;
+				size--;
+
+				if (isEmpty()) head = nullptr;
+				return tdata;
+			}
+			/*else throw empty list*/
+		}
+		
 		// removeAt
+		// contains
 		// remove
 
 	};
