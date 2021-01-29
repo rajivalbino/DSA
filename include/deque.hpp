55f1f6f485f9be6c464ebd21e443ee2b44f31def
Rajiv Albino <rajivalbino@gmail.com>
2021-01-28 22:09:40 -0300

    implementation using doublylinkedlist

--- include/deque.hpp
+++ include/deque.hpp
@@ -16,7 +16,14 @@ namespace rds {
 		~Deque() { list.clear(); }
 
 		inline bool isEmpty()	const { return list.isEmpty(); }
-		inline int  size()		const { return list.size_(); }
-		// ...
+		inline int size()		const { return list.size_(); }
+		inline int peekFront()	const { return list.head_(); }
+		inline int peekBack()	const { return list.tail_(); }
+
+		inline void push(int d) { list.addHead(d); }
+		inline int pop() { return list.removeHead(); }
+		inline void inject(int d) { list.addTail(d); }
+		inline int eject() { return list.removeTail(); }
+		inline void popAll() { list.clear(); }
 	};
 }
