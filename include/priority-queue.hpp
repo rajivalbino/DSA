8375c20598879c29460d29284887839f01b97271
Rajiv Albino <rajivalbino@gmail.com>
2021-02-17 20:54:31 -0300

    dynamic array initialization copy

--- include/priority-queue.hpp
+++ include/priority-queue.hpp
@@ -1,35 +1,98 @@
 // Priority Queue
 // int datatype
 
-#include "dynamic-array.hpp"
-
 #pragma once
 
 namespace rds {
 	template<typename T>
 	class PriorityQueue {
-		DynamicArray<T> _array;
+
+	private:
+		T* _data;
+		size_t _size;
+		size_t _cap;
 
 	public:
+		PriorityQueue(size_t c = 8) : _size(0), _cap(c), _data(nullptr) {
+			reserve(_cap);
+		}
 
-		bool empty() const {}
-		size_t size() const {}
-		size_t capacity() const {}
+		~PriorityQueue() {
+			clear();
+			::operator delete(_data, _cap * sizeof(T));
+		}
 
-		void clear() {}
+		PriorityQueue(const PriorityQueue& da)                = delete;
+		PriorityQueue& operator=(PriorityQueue da)            = delete;
+		PriorityQueue(PriorityQueue&& da)            noexcept = delete;
+		PriorityQueue& operator=(PriorityQueue&& da) noexcept = delete;
 
-		void reserve() {}
+		bool   empty()    const { return (_size == 0); }
+		size_t size()     const { return _size; }
+		size_t capacity() const { return _cap; }
 
-		void insert(const T& d) {}
+		void clear() {
+			for (size_t i = 0; i < _size; ++i)
+				_data[i].~T();
 
-		void insert(T&& d) {}
+			_size = 0;
+		}
+
+		void reserve(size_t c) {
+			auto oldData = _data;
+
+			_data = (T*)::operator new(c * sizeof(T));
+
+			if (_size > c)
+				_size = c;
+
+			for (size_t i = 0; i < _size; ++i)
+				_data[i] = std::move(oldData[i]);
+
+			for (size_t i = 0; i < _size; ++i)
+				oldData[i].~T();
+
+			::operator delete(oldData, _cap * sizeof(T));
+			_cap = c;
+		}
+
+		void insert(const T& d) {
+			if (_size >= _cap)
+				reserve(_cap << 1);
+
+			_data[_size++] = d;
+		}
+
+		void insert(T&& d) noexcept {
+			if (_size >= _cap)
+				reserve(_cap << 1);
+
+			_data[_size++] = std::move(d);
+		}
 		
 		template<typename... Args>
-		void emplace(Args&&... args) {}
+		void emplace(Args&&... args) {
+			if (_size >= _cap)
+				reserve(_cap << 1)
 
-		T peek() const {}
+				new(&_data[_size++]) T(std::forward<Args>(args)...);
+		}
+
+		T peek() const {
+			if (empty())
+				throw 0; /*empty list*/
+			else
+				return _data[0];
+		}
 		
-		T poll() {}
+		T poll() {
+			if (empty())
+				throw 0; /*empty list*/
+			else {
+				auto tdata = _data[0];
+
+			}
+		}
 
 		bool remove(const T& d) {}
 
